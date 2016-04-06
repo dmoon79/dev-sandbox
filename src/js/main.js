689773a35194215750a19beba5b84f246def7845
Derek Moon <dmoon@dmoon-mac.local>
2016-04-05 18:24:10 -0700

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-use strict;
+'use strict';
+
 $(document).ready(function() {
-  //  $('.sb-input').hide();
-    $('.sb-submit').hover(function(){
-      $('.sb-input').show('slow');
-    }, function(){
-      $('.sb-input').hide('slow');
-    });
+  $('.sb-submit').hover(function(){
+    $('.sb-input').show('slow');
+  }, function(){
+    $('.sb-input').hide('slow');
+  });
 });
